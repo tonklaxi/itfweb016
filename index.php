389788dd248d04457785ac051e5f9d879cb2e0f7
tonklaxi <73679671+tonklaxi@users.noreply.github.com>
2020-10-30 10:41:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
-<?php
-echo "<h2>PHP is Fun!</h2>";
-echo "Hello world!<br>";
-?>
+<html>
+    <head>
+        <title>Hello world</title>
+    </head>
+    <body>
+        <h1>My First Heading</h1>
+        <p>My first Paragraph</p>
+        <img src="cat.jpg">
+        <br>
+        <a href= "http://www.it.kmitl.ac.th"> This is a Link</a>
+
+        <h2>The unordered list</h2>
+        <ul>
+            <li>Google</li>
+            <li>Yahoo</li>
+            <li>Amazom</li>
+        </ul>
+
+    </body>
+</html>
