4d26f6b6f8db8a5f20ad04d68daf0206b8d6e424
tonklaxi <73679671+tonklaxi@users.noreply.github.com>
2020-10-30 11:00:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <body>
         <h1>Ballondor2020!</h1>
         <p>Jessie Lingard win Ballondor2020</p>
-        <img src="jessie.jpg">
-        <img src="jessie2.jpg">
+        <img src="jessie.jpg" width="300" height="300">
+        <img src="jessie2.jpg" width="300" height="300">
         <br>
         <a href= "https://www.manutd.com/"> Manu official website</a>
         <h2>Breaking news!!!</h2>
