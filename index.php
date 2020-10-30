e67f52a75aefc6c0259488e4126c782aee7a1851
tonklaxi <73679671+tonklaxi@users.noreply.github.com>
2020-10-30 10:54:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,14 @@
     </head>
     <body>
         <h1>Ballondor2020!</h1>
-        <p>Jessie Lingard win ballondor2020</p>
+        <p>Jessie Lingard win Ballondor2020</p>
         <img src="jessie.jpg">
         <img src="jessie2.jpg">
         <br>
         <a href= "https://www.manutd.com/"> Manu official website</a>
-        <p>Breaking news!!! ข่าวใหญ่เจซซี่ ลินการ์ด กองกลางตัวทำเกมของแมนเชสเตอร์ยูไนเต็ด
-            คว้ารางวัลบัลลองดอร์ปี2020! </p>
+        Breaking news!!! ข่าวใหญ่เจซซี่ ลินการ์ด กองกลางตัวทำเกมของแมนเชสเตอร์ยูไนเต็ด
+        คว้ารางวัลบัลลองดอร์ปี2020!
+        
         <p>นาย จักราภัทร สุดใจ 63070016</p>
 
     </body>
