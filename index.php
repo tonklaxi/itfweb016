2c02440182c5516526cfffd871a34107172c046d
tonklaxi <73679671+tonklaxi@users.noreply.github.com>
2020-11-06 16:04:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,51 @@
-<html>
-    <head>
-        <title>Ballondor2020!</title>
-    </head>
-    <body>
-        <h1>Ballondor2020!</h1>
-        <p>Jessie Lingard win Ballondor2020</p>
-        <img src="jessie.jpg" width="300" height="300">
-        <img src="jessie2.jpg" width="300" height="300">
-        <br>
-        <a href= "https://www.manutd.com/"> Manu official website</a>
-        <h2>Breaking news!!!</h2>
-        <p>ข่าวใหญ่เจซซี่ ลินการ์ด กองกลางตัวทำเกมของแมนเชสเตอร์ยูไนเต็ด
-            คว้ารางวัลบัลลองดอร์ปี2020!</p>
-        
-        <h3>นาย จักราภัทร สุดใจ ต้นกล้า 63070016</h3>
-
-    </body>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Hello</title>
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+      integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
+      crossorigin="anonymous"
+    />
+  </head>
+  <body>
+    <div class="container">
+        <div class="row">
+        <div class="col">
+        <div class="card">
+            <img class="card-img-top" src="staff6.jpg" >
+            <div class="card-body">
+            <h4 class="card-title">anonymous</h4>
+            <p class="card-text">He handsome but less than me<p>
+            <a href="#" class="btn btn-primary">Read more</a>
+        </div>
+        </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img class="card-img-top" src="staff10.jpg" >
+                <div class="card-body">
+                <h4 class="card-title">anonymous</h4>
+                <p class="card-text">He handsome but less than me<p>
+                <a href="#" class="btn btn-primary">Read more</a>
+            </div>
+            </div>
+            </div>
+        <div class="col">
+        <div class="card">
+            <img class="card-img-top" src="staff9.jpg" >
+            <div class="card-body">
+            <h4 class="card-title">anonymous</h4>
+            <p class="card-text">He handsome but less than me<p>
+            <a href="#" class="btn btn-primary">Read more</a>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+  </body>
 </html>
